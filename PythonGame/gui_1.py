3f5b7bc1fd53618edfea8e0cac146d1c9cf67f44
PJNeary406 <pj.neary23@gmail.com>
2016-05-03 13:51:09 -0600

    frist commit

---
+++ PythonGame/gui_1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+root.geometry("300x300")
+
+#`oneLabel = Label(root,text="weeee",fg="black",bg="green").pack(side=TOP)
+oneLabel = Label(root,text="weeee",fg="black",bg="green").place(x=30,y=60)
+
+root.mainloop()
+
