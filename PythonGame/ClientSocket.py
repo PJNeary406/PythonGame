6d725dc4273559078ee6909d2eff665904e2b63b
PJNeary406 <pj.neary23@gmail.com>
2016-05-12 16:48:59 -0600

    Added worms_client.py and made ClientSocket OO

--- PythonGame/ClientSocket.py
+++ PythonGame/ClientSocket.py
@@ -3,31 +3,34 @@ Created on Mar 17, 2016
 
 @author: stephen.neary
 '''
-import socket 
+import socket
 import SocketServer
 from threading import Thread
 
-SERVER_LOCATION = ("localhost" , 6969)
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-message = ""
+class ClientSocket():
 
-if __name__ == '__main__':
-    sock.connect(SERVER_LOCATION)
-    print "Connecting to server %s port %s" % SERVER_LOCATION
-    
-    try:
-        while message != "END":
-            message = raw_input("Enter message:")
-            sock.sendall(message)
-            
-            data = sock.recv(1024)
-            
-            if data:
-                print "Server responce was :",data
-            
-            if data == "END":
-                break;
-            
-    finally:
-        sock.close()  
-       
+    def __init__(self):
+        self.SERVER_LOCATION = ("localhost", 6969)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_response = ""
+
+
+    def connect(self):
+        self.sock.connect(self.SERVER_LOCATION)
+        print "Connecting to server %s port %s" % self.SERVER_LOCATION
+
+    def send_message(self, data):
+        try:
+            self.sock.sendall(data)
+
+            self.server_response = self.sock.recv(1024)
+
+            if self.server_response:
+                print "Server response was :", self.server_response
+
+        finally:
+            self.sock.close()
+
+    def get_response(self):
+
+        return self.server_response
